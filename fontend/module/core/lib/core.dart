c685a01ebddad0b81385d0092aec29b052e7eba8
Đỗ Mạnh Tiến <tien310001@gmail.com>
2025-10-06 10:19:02 +0700

    Create: Create core module

---
+++ fontend/module/core/lib/core.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+export 'package:core/core.dart';
+
+class CoreApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
