73baaed6fa354a187e5407116c1e0f8086b89ccf
Đỗ Mạnh Tiến <tien310001@gmail.com>
2025-10-15 14:31:28 +0700

    update: logic auth

---
+++ fontend/module/auth/lib/src/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,6 @@
+import 'package:auth/src/presentation/bloc/auth_event.dart';
+import 'package:auth/src/presentation/bloc/auth_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+class AuthBloc extends Bloc<AuthEvent,AuthState> {
+  AuthBloc(super.initialState);
+}
---
+++ fontend/module/auth/lib/src/presentation/ui/widget/login-form.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginForm extends StatelessWidget{
+  const LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
