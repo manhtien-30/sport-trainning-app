c7df2cb3719a4354780b85a7ce545c736fd472c0
Đỗ Mạnh Tiến <tien310001@gmail.com>
2025-12-16 01:55:12 +0700

    update: main app

--- fontend/lib/main.dart
+++ fontend/lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("d"),
+    return GetMaterialApp(
+      enableLog: true,
+      debugShowCheckedModeBanner: false,
+      title: "App Demo",
+      initialRoute: "/",
+
     );
   }
 }
